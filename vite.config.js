ce906b9d180a5bb79971af2a43a1801e0d782ce0
Siderea <hutornoyaa@gmail.com>
2025-05-03 09:39:47 +0300

    rename base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/ProductsReact/',
+	base: 'https://sidereah.github.io/ProductsReact',
 	plugins: [react(), tailwindcss()],
 })
