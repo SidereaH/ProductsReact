d7c3a6318c773c0c2fb3575151557d70bed0abd7
Siderea <hutornoyaa@gmail.com>
2025-05-03 09:24:32 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import ProductsPage from './pages/ProductPage'
 import ProductDetailPage from './pages/ProductDetailPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => (
-	<Router>
-		<Routes>
-			<Route path='/products' element={<ProductsPage />} />
-			<Route path='/product/:id' element={<ProductDetailPage />} />
-			<Route path='*' element={<NotFoundPage />} />
-		</Routes>
-	</Router>
+	<Routes>
+		<Route path='/products' element={<ProductsPage />} />
+		<Route path='/product/:id' element={<ProductDetailPage />} />
+		<Route path='*' element={<NotFoundPage />} />
+	</Routes>
 )
 
 export default App
